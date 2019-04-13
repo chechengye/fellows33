6c2af988d3e1ad8615ecda4f265fe9bf90984312
chechengye <262926400@qq.com>
2019-04-13 16:19:11 +0800

    day03

--- day03Demo/src/Test_10.java
+++ day03Demo/src/Test_10.java
@@ -11,6 +11,14 @@ public class Test_10 {
         cm.addChicken(new Chicken(5 , "小鸡5" , 5 , 6));
         cm.addChicken(new Chicken(6 , "小鸡6" , 5 , 6));
         cm.addChicken(new Chicken(7 , "小鸡7" , 5 , 6));
+      /*  cm.printInfo();
+        System.out.println("----------------");
+        cm.updateChicken(new Chicken(2 , "呜呜" , 11, 55));
+        cm.printInfo();
+        System.out.println("--------------");
+        System.out.println(cm.getChickenById(3));
+        System.out.println("--------------");*/
+        cm.deleteChickenById(2);
         cm.printInfo();
     }
 }
@@ -34,8 +42,48 @@ class ChickenManage{
     }
 
     /**
-     *
+     * 更新鸡
      */
+    public void updateChicken(Chicken c){
+        for(Chicken cs : chickens){
+            if(cs.getId() == c.getId()){
+                cs.setName(c.getName());
+                cs.setWeight(c.getWeight());
+                cs.setAge(c.getAge());
+            }
+        }
+    }
+
+    /**
+     * 删除一只鸡，根据ID
+     */
+    public void deleteChickenById(int id){
+        for(int i = 0 ; i < chickens.length ; i++){
+            if(chickens[i].getId() == id){
+                for(int j = i ; j < count ; j++){
+                    if(j + 1 == count){
+                        break;
+                    }
+                    chickens[j] = chickens[j+1];
+                }
+
+            }
+        }
+        chickens[chickens.length - 1] = null;
+    }
+
+    /**
+     * 查询一只鸡根据ID
+     */
+    public Chicken getChickenById(int id){
+        for(Chicken cs : chickens) {
+            if (cs.getId() == id) {
+                return cs;
+            }
+        }
+        return null;
+    }
+
     /**
      * 打印所有鸡
      */
