12a48e005e6408db2ceddd8556a247c72a7ff071
chechengye <262926400@qq.com>
2019-04-27 10:35:35 +0800

    day07

--- day07Demo/src/Test_02.java
+++ day07Demo/src/Test_02.java
@@ -1,17 +1,69 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Test_02 {
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(50);
+        //Vector<String> list = new Vector<>(50);
         list.add("tom");
         list.add("lily");
         list.add("lucy");
         list.add("abm");
-        iterator(list);
+        //iterator(list);
+        //enumeration(list);
+        //foreach(list);
+
+        //System.out.println(testFunction("abc", (s) -> s.toUpperCase()));
+     /*   Supplier<String[]> s = new Supplier<String[]>() {
+            @Override
+            public String[] get() {
+                return new String[0];
+            }
+        };*/
+        String[] strings = testSupplier(10, () -> {
+            String[] ss = new String[10];
+            for (int i = 0; i < ss.length; i++) {
+                ss[i] = (Math.random() * 100) + "";
+            }
+            return ss;
+        });
+        for(String s : strings){
+            System.out.println(s);
+        }
+
+    }
+
+    private static String[] testSupplier(int num , Supplier<String[]> s){
+        for(int i = 0 ; i< num ; i++){
+            return s.get();
+        }
+        return null;
+    }
+
+    private static String testFunction(String val , Function<String , String> f){
+        return f.apply(val);
+    }
+
+    private static void foreach(Collection<String> c){
+        /*for(String s : c){
+            System.out.println(s);
+        }*/
+        MyConsumer tConsumer = s -> System.out.println(s);
+
+        c.forEach(tConsumer);
+        //c.forEach(System.out::println);
+
+
+    }
+
+    public static void enumeration(Vector<String> v){
+        Enumeration<String> elements = v.elements();
+        while (elements.hasMoreElements()){
+            System.out.println(elements.nextElement());
+        }
     }
 
     public static void iterator(Collection<String> c){
@@ -20,4 +72,7 @@ public class Test_02 {
             System.out.println(iterator.next());
         }
     }
+}
+interface MyConsumer extends Consumer<String>{
+
 }
